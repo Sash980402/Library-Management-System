25eb1ef056330074c811a22852ca2ce78385b813
Sash980402 <kesharasasanka120@gmail.com>
2025-01-17 00:03:12 +0530

    new

--- src/main/java/org/example/library_management_system/controller/LoginFormController.java
+++ src/main/java/org/example/library_management_system/controller/LoginFormController.java
@@ -1,7 +1,25 @@
 package org.example.library_management_system.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class LoginFormController {
 
+    public TextField txtUserName;
+    public TextField txtPassword;
+    public AnchorPane mainsubAnchorPane;
+    public AnchorPane subAnchorPane1;
+    public AnchorPane subAnchorPane2;
+    public PasswordField pwfPassword;
+
+    public void btnViewAndHideOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/org/example/library_management_system/controller/sub/ReturnBookFormController.java
+++ src/main/java/org/example/library_management_system/controller/sub/ReturnBookFormController.java
@@ -1,4 +1,26 @@
 package org.example.library_management_system.controller.sub;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class ReturnBookFormController {
+    public TableView tblNotReturnbookRecords;
+    public TableColumn colRecordId;
+    public TableColumn colBookid;
+    public TableColumn colBookName;
+    public TableColumn colMemberId;
+    public TableColumn colMemberName;
+    public TableColumn colReturnDate;
+    public TextField txtSearchKeyword;
+    public RadioButton rbBookId;
+    public RadioButton rbMemberId;
+    public RadioButton rbMemberMobileNumber;
+    public Label lblLateDateCount;
+    public TextField txtFineForOneDay;
+    public Label lblFine;
+    public TextField txtPayment;
+    public Label lblBalance;
+
+    public void btnMarkAsReturnedOnAction(ActionEvent actionEvent) {
+    }
 }
