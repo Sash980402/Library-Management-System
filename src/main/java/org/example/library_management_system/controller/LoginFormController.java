022f4c0d2d8d9b5265f969dddc6b21eba50949ac
Sash980402 <kesharasasanka120@gmail.com>
2025-01-17 00:36:19 +0530

    new

--- src/main/java/org/example/library_management_system/controller/LoginFormController.java
+++ src/main/java/org/example/library_management_system/controller/LoginFormController.java
@@ -1,9 +1,16 @@
 package org.example.library_management_system.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class LoginFormController {
 
@@ -21,5 +28,16 @@ public class LoginFormController {
     }
 
     public void btnCreateAnAccountOnAction(ActionEvent actionEvent) {
+        subAnchorPane2.getChildren().clear();
+
+        try {
+            Node load = FXMLLoader.load(getClass().getResource("/view/register_form_sub.fxml"));
+            ObservableList<Node> children = subAnchorPane2.getChildren();
+            children.add(load);
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR,"Failed to load the form");
+            e.printStackTrace();
+        }
+
     }
 }
