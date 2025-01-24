2c57440b20588ac90431703936f901089f4ec698
sasanka <120694928+Sash980402@users.noreply.github.com>
2025-01-24 19:49:12 +0530

    Update Starter.java
    new

--- src/main/java/org/example/library_management_system/Starter.java
+++ src/main/java/org/example/library_management_system/Starter.java
@@ -2,7 +2,7 @@ package org.example.library_management_system;
 
 public class Starter {
     public static void main(String[] args) {
-	System.out.println("hello");
+	
         AppInitializer.main(args);
     }
 }
