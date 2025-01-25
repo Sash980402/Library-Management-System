9f5d275dfcfe91dad26133e8b7fb6bfa58f2904a
Sash980402 <kesharasasanka120@gmail.com>
2025-01-25 16:47:59 +0530

    NEW

---
+++ src/main/java/org/example/library_management_system/repo/custom/CategoryRepo.java
@@ -0,0 +1,7 @@
+package org.example.library_management_system.repo.custom;
+
+import org.example.library_management_system.entity.Category;
+import org.example.library_management_system.repo.CrudRepository;
+
+public interface CategoryRepo extends CrudRepository<Category, Integer> {
+}
